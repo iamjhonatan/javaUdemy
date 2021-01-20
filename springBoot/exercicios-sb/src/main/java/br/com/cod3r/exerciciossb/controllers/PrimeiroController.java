94d26fa3fdb9f8f401c63184446a804f6fcd7c55
Jhonatan Medeiros <55262464+iamjhonatan@users.noreply.github.com>
2021-01-20 18:21:46 -0300

    Inicio do módulo Spring Boot.

---
+++ springBoot/exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/PrimeiroController.java
@@ -0,0 +1,18 @@
+package br.com.cod3r.exerciciossb.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//mapeando classes e metodos nas urls/ considerando a classe como uma api rest controller
+@RestController
+public class PrimeiroController {
+	
+	@GetMapping(path = {"/ola", "/saudacao"})
+	// mapeando uma requisicao para esse metodo
+	public String ola() {
+		return "Olá SpringBoot!";
+	}
+
+}
