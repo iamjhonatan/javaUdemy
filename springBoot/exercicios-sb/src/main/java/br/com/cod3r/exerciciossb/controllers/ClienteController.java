ed69976c10661fc5676d4321c5a84858f7ffe312
Jhonatan Medeiros <55262464+iamjhonatan@users.noreply.github.com>
2021-01-21 00:19:31 -0300

    Web Service retornando um objeto.

---
+++ springBoot/exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
@@ -0,0 +1,18 @@
+package br.com.cod3r.exerciciossb.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cod3r.exerciciossb.models.Cliente;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+	
+	@GetMapping(path = "/qualquer")
+	public Cliente obterCliente() {
+		return new Cliente (28, "Pedro", "123.456.789-89");
+	}
+
+}
--- springBoot/exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/PrimeiroController.java
+++ springBoot/exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/PrimeiroController.java
@@ -1,6 +1,7 @@
 package br.com.cod3r.exerciciossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
